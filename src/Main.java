3f37699f56cf95a6592b64d87c57b12b0b4e6b21
Arzymatova Nurperi <84436333+ArzymatovaNurperi@users.noreply.github.com>
2022-10-05 21:51:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person=new Person();
+        person.name="Abdykadyr";
+        person.dateOfBirth=1996;
+        person.getAge();
+
+        Person person1=new Person();
+        person1.name="Kurmanbek";
+        person1.dateOfBirth=1989;
+        person1.getAge();
+    }
+}
---
+++ src/University.java
@@ -0,0 +1,11 @@
+public class University {
+    String name;
+    int yearOfStart;
+    int countOfCorpus;
+    int countOfFloor;
+    String country;
+
+    public void teach(){
+        System.out.println("We can study at University");
+    }
+}
